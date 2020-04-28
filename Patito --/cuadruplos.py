e27ca2649cf739e8d0e5ce603408bfda4e877307
Noe <noe9704@hotmail.com>
2020-04-27 20:47:40 -0500

    Prueba cuadruplos

--- Patito --/cuadruplos.py
+++ Patito --/cuadruplos.py
@@ -1,10 +1,45 @@
 
+pilaOperadores = []
+pilaTipos = []
+pilaId = []
+opDer = 0
+opIzq = 0
+operador = 0
+
+opsEXP = {
+    "+",
+    "-",
+    "*",
+    "/"    
+}
 
 
 def eval(cuadruplo):
-    print(cuadruplo)
+    tokens = cuadruplo.split()
+    for token in tokens:
+        if(token in opsEXP):
+            pilaOperadores.append(token)
+        else:
+            pilaId.append(token)
+        if(pilaOperadores[-1] == "*" or pilaOperadores[-1] == "/"):
+            opDer = pilaId.pop()
+            opIzq = pilaId.pop()
+
+            operador = pilaOperadores.pop()
+            generaCuadruplo(operador, opDer, opIzq, "12")
+            
+
+def generaCuadruplo(operador , opIzq, opDer , respuesta):
+    print(operador,opIzq,opDer,respuesta)
+
+
+    
+    ##print(pilaOperadores)
+    ##print(pilaId)
+    
 
 
 
 
-eval('A+B+C')
+
+eval('4 + 5 * 6')
