7409d33e226f8e99987736b98ff7dcc656f4071a
MarceloFlores <luismarcelofc@gmail.com>
2020-05-30 19:45:38 -0500

    Avance memoria virtual

--- Patito --/memoria.py
+++ Patito --/memoria.py
@@ -1,41 +1,179 @@
+# ------------------------------------------------------------
+# Luis Marcelo Flores Canales A01280943
+# ------------------------------------------------------------
 
-# Tipos de bases para guardar direcciones
+import sys
+
+## Tipos de bases para guardar direcciones
 INT_BASE = 0
 FLOAT_BASE = 1000
 CHAR_BASE = 2000
 BOOL_BASE = 3000
 
-# direcciones locales y globales
+## Direcciones locales y globales
 GLOBAL_BASE = 0
 LOCAL_BASE = 10000
 CTE_BASE = 20000
 MAX_BASE = 30000
 
-##Direcciones locales
+## Direcciones para guarda locales
 local_int = LOCAL_BASE + INT_BASE
 local_float = LOCAL_BASE + FLOAT_BASE
 local_char = LOCAL_BASE + CHAR_BASE
 local_bool = LOCAL_BASE + BOOL_BASE
 
-##Direcciones globales
+## Direcciones para guardar globales
 global_int = GLOBAL_BASE + INT_BASE
 global_float = GLOBAL_BASE + FLOAT_BASE
 global_char = GLOBAL_BASE + CHAR_BASE
-global_bool = GLOBAL_BASE + global_bool
+global_bool = GLOBAL_BASE + BOOL_BASE
 
 
-#Direcciones de constantes
+## Direcciones para guardar constantes
 constante_int = CTE_BASE + INT_BASE
 constante_float = CTE_BASE + FLOAT_BASE
 constante_char = CTE_BASE + CHAR_BASE
 constante_bool = CTE_BASE + BOOL_BASE
 
-symbols = {}
-quadruples = []
-constants = {}
-memoriaGlobal = Memoria()
-memoriaLocales = []
+## Clase memoria
 
+class Memoria:
+    def __init__(self):
+        self.ints = {}
+        self.floats = {}
+        self.chars = {}
+        self.bools = {}
+    
+    def __repr__(self):  
+        return "Integer:% s Float:% s Char:% s  bool:% s " % (self.ints, self.floats, self.chars,self.bools) 
+
+    def inserta_Dir_Globales(self,var,tipo):
+        global global_int, global_float, global_char, global_bool
+        if tipo == "int":
+            direccion = global_int
+            global_int += 1
+            self.ints[direccion] = None
+            return direccion
+        elif tipo == "float":
+            direccion = global_float
+            global_float += 1
+            self.floats[direccion] = None
+            return direccion
+        elif tipo == "char":
+            direccion = global_char
+            global_char += 1
+            self.char[direccion] = None
+            return direccion
+        elif tipo == "bool":
+            direccion = global_bool
+            global_bool += 1
+            self.bools[direccion] = None
+            return direccion
+
+    def inserta_Dir_Constantes(self, const,tipo):
+        global constante_int, constante_float, constante_char, constante_bool
+        if tipo == "int":
+            direccion = constante_int
+            constante_int += 1
+            print(constante_int)
+            self.ints[direccion] = const
+            return direccion
+        elif tipo == "float":
+            direccion = constante_float
+            constante_float += 1
+            self.floats[direccion] = const
+            return direccion
+        elif tipo == "char":
+            direccion = constante_char
+            constante_char += 1
+            self.chars[direccion] = const
+            return direccion
+        elif tipo == "bool":
+            direccion = constante_bool
+            constante_bool += 1
+            self.bools[direccion] = const
+            return direccion
+
+    def inserta_Dir_Locales(self,var,tipo):
+        global local_int, local_float, local_char, local_bool
+        if tipo == "int":
+            direccion = local_int
+            local_int += 1
+            self.int[direccion] = var
+            return direccion
+        elif tipo == "float":
+            direccion = local_float
+            local_float += 1
+            self.float[direccion] = var
+            return direccion
+        elif tipo == "char":
+            direccion = local_char
+            local_char += 1
+            self.char[direccion] = var
+            return direccion
+        elif tipo == "bool":
+            direccion = local_bool
+            local_bool += 1
+            self.bool[direccion] = var
+            return direccion
+    
+    def inserta_Dir_temporales(self,var,tipo):
+        global local_int, local_float, local_char, local_bool
+        if tipo == "int":
+            direccion = local_int
+            local_int += 1
+            self.int[direccion] = var
+            return direccion
+        elif tipo == "float":
+            direccion = local_float
+            local_float += 1
+            self.float[direccion] = var
+            return direccion
+        elif tipo == "char":
+            direccion = local_char
+            local_char += 1
+            self.char[direccion] = var
+            return direccion
+        elif tipo == "bool":
+            direccion = local_bool
+            local_bool += 1
+            self.bool[direccion] = var
+            return direccion
+
+    def getType(self,varAddress):
+        if (int(varAddress) / 1000) % 10 == 0:
+            return 'int'
+        elif (int(varAddress) / 1000) % 10 == 1:
+            return 'float'
+        elif (int(varAddress) / 1000) % 10 == 2:
+            return 'char'
+        elif (int(varAddress) / 1000) % 10 == 3:
+            return 'bool'
+    
+    def getValue(self,address):
+        if (int(address) / 1000) % 10 < 1:
+            for key in self.ints :
+                if address == key :
+                    return self.ints[key]
+        elif (int(address) / 1000) % 10 < 2:
+            for key in floats:
+                if address == key :
+                    return self.floats[key]
+        elif (int(address) / 1000) % 10 < 3:
+            for key in char:
+                if address == key :
+                    return self.char[key]
+        elif (int(address) / 1000) % 10 < 4:
+            for key in bool:
+                if address == key :
+                    return self.bool[key]
+        
+#print(symbols)
+#print(quadruples)
+#print(constants)
+"""
+
+"""
 def asigna(direccion,valor):
     if direccion < CTE_BASE and direccion > LOCAL_BASE :
         memoriaLocales[-1] = valor
@@ -43,107 +181,13 @@ def asigna(direccion,valor):
         memoriaGlobal[-1] = valor
 
 
-
 def asignaParametros():
     pass
-
-
-if len(sys.argv) is not 2:
-    print('Error, leyendo el objeto del archivo')
-    raise SyntaxError('Patito necesita de un archivo')
-else: 
-    data = sys.argv[1]
-    with open(data, 'r', newline='\n') as file:
-        input = eval(file.read())
-        global symbols, quadruples, constants
-        symbols = input['symbols']
-        quadruples = input['quadruples']
-        constants = input['constants']
-        run()
-
-
-def inserta_Dir_Locales(var,tipo):
-    global local_int, local_float, local_char, local_bool
-    if tipo == "int":
-        direccion = local_int
-        local_int += 1
-        memoriaLocales.append(var)
-        return direccion
-    elif tipo == "float":
-        direccion = local_float
-        local_float += 1
-        memoriaLocales.append(var)
-        return direccion
-    elif tipo == "char":
-        direccion = local_char
-        local_char += 1
-        memoriaLocales.append(var)
-        return direccion
-    elif tipo == "bool":
-        direccion = local_bool
-        local_bool += 1
-        memoriaLocales.append(var)
-        return direccion
-    
-
-def inserta_Dir_Constantes(self, const,tipo):
-    global constante_int, constante_float, constante_char, constante_bool
-    if tipo == "int":
-        direccion = constante_int
-        constante_int += 1
-        constants[direccion] = const
-        return direccion
-    elif tipo == "float":
-        direccion = constante_float
-        constante_float += 1
-        constants[direccion] = const
-        return direccion
-    elif tipo == "char":
-        direccion = constante_char
-        constante_char += 1
-        constants[direccion] = const
-        return direccion
-    elif tipo == "bool":
-        direccion = constante_bool
-        constante_bool += 1
-        constants[direccion] = const
-        return direccion
-
-
-class Memoria :
-    def __init__(self):
-        self.ints = {}
-        self.floats = {}
-        self.chars = {}
-        self.bools = {}
-
-    def inserta_Dir_Globales(self,var,tipo):
-        global global_int, global_float, global_char, global_bool
-        if tipo == "int":
-            direccion = global_int
-            global_int += 1
-            self.ints[direccion] = var
-            return direccion
-        elif tipo == "float":
-            direccion = global_float
-            global_float += 1
-            self.floats[direccion] = var
-            return direccion
-        elif tipo == "char":
-            direccion = global_char
-            global_char += 1
-            self.char[direccion] = var
-            return direccion
-        elif tipo == "bool":
-            direccion = global_bool
-            global_bool += 1
-            self.bools[direccion] = var
-            return direccion
     
 
 ####Logica, no hacer mucho caso
 def run():
-    if quadruples[-1] = "+":
+    if quadruples[-1] == "+":
         operand = quadruples[-1]
         opIzq = quadruples[-1]
         opDer = quadruples[-1]
--- Patito --/virtualMachine.py
+++ Patito --/virtualMachine.py
@@ -1,22 +1,79 @@
+import memoria
+import sys
+
+symbols = {}
+quadruples = []
+constants = {}
+
+memoriaGlobal = memoria.Memoria()
+memoriaConstante = memoria.Memoria()
+memoriaLocales = []
+
+if len(sys.argv) != 2:
+    print('Error, leyendo el objeto del archivo')
+    raise SyntaxError('Necesitas enviar un archivo .txto correcto')
+else: 
+    data = sys.argv[1]
+    with open(data, 'r', newline='\n') as file:
+        input = eval(file.read())
+        symbols = input['symbols']
+        quadruples = input['quadruples']
+        constants = input['constants']
+        #run()
+
+for key in symbols['global']['vars'].keys():
+    keyAddress = symbols['global']['vars'][key]['address']
+    keyType = symbols['global']['vars'][key]['type']
+    memoriaGlobal.inserta_Dir_Globales(keyAddress,keyType)
+
+for key in constants.keys():
+    keyValue = key
+    keyType = constants[key]['type']
+    memoriaConstante.inserta_Dir_Constantes(keyValue,keyType)
 
 
-cuadruplo = [["+",10001,10002,10003],["-",10004,10005,10006],["*",10007,10008,10009]]
 
-def ejecuta():
-    global cuadruplo 
-    cuad = cuadruplo[0]
+print("Memoria Virtual")
+print(memoriaGlobal)
+print("Constantes")
+print(memoriaConstante)
+
+# quadruples = [["+",10001,10002,10003],["-",10004,10005,10006],["*",10007,10008,10009]]
+
+def ejecuta(cuad,pos):
+    currentERA = 'main'
+    if(cuad[0] == 'GOTO'): 
+        return int(cuad[3])
+
+    if(cuad[0] == 'ENDPROC'):
+        return pos + 1 
+
     if cuad[0] == '+':
-        opIzq = cuad[1]
+        memTem = memoria.Memoria()
+        opIzq = cuad[1] # Direccion de memoria 1
         opDer = cuad[2]
-        valorIzq = opIzq
-        valorDer = opDer
-        nuevoResultado = valorIzq + valorDer
-        ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
-        ##contador
-        ##return contador
-        
-        print(cuadruplo[0])
+
+        type1=memoriaGlobal.getType(opIzq)
+        type2=memoriaGlobal.getType(opDer)
+
+        #valorIzq = opIzq # Traer datos de la direccion
+        #valorDer = opDer
+        print(memoriaConstante.getValue(opIzq))
+        if(type1 == 'int') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'float') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'char') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'bool') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        print("Valor")
+        print(valorIzq)
+        #valorIzq = memoriaGlobal
+
+        #nuevoResultado = valorIzq + valorDer # Operacion de los valores
+
+        #memoriasTemporales.append(memTem)
     elif cuad[0] == '-':
         opIzq = cuad[1]
         opDer = cuad[2]
@@ -25,11 +82,12 @@ def ejecuta():
         valorDer = opDer
         nuevoResultado = valorIzq - valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador
         
-        print(cuadruplo[0])
+        print(quadruples[0])
+    
     elif cuad[0] == '*':
         opIzq = cuad[1]
         opDer = cuad[2]
@@ -38,11 +96,11 @@ def ejecuta():
         valorDer = opDer
         nuevoResultado = valorIzq * valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador
         
-        print(cuadruplo[0])
+        print(quadruples[0])
     elif cuad[0] == '/':
         opIzq = cuad[1]
         opDer = cuad[2]
@@ -51,11 +109,11 @@ def ejecuta():
         valorDer = opDer
         nuevoResultado = valorIzq / valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador
         
-        print(cuadruplo[0])
+        print(quadruples[0])
     elif cuad[0] == '<':
         opIzq = cuad[1]
         opDer = cuad[2]
@@ -65,11 +123,11 @@ def ejecuta():
         valorDer = 0
         nuevoResultado = valorIzq < valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador
         
-        print(cuadruplo[0])
+        print(quadruples[0])
     elif cuad[0] == '>':
         opIzq = cuad[1]
         opDer = cuad[2]
@@ -79,10 +137,10 @@ def ejecuta():
         valorDer = 0
         nuevoResultado = valorIzq > valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador        
-        print(cuadruplo[0])
+        print(quadruples[0])
 
     elif cuad[0] == '<=':
         opIzq = cuad[1]
@@ -93,10 +151,10 @@ def ejecuta():
         valorDer = 0
         nuevoResultado = valorIzq <= valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador        
-        print(cuadruplo[0])
+        print(quadruples[0])
 
     elif cuad[0] == '>=':
         opIzq = cuad[1]
@@ -107,10 +165,10 @@ def ejecuta():
         valorDer = 0
         nuevoResultado = valorIzq <= valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador        
-        print(cuadruplo[0])
+        print(quadruples[0])
 
     elif cuad[0] == '==':
         opIzq = cuad[1]
@@ -121,26 +179,46 @@ def ejecuta():
         valorDer = 0
         nuevoResultado = valorIzq == valorDer
         ##actualiza el resultado del cuadruplo  con nuevoResultado
-        cuadruplo.pop(0)
+        quadruples.pop(0)
         ##contador
         ##return contador        
-        print(cuadruplo[0])
+        print(quadruples[0])
        
     elif cuad[0] == '=':
-        '''
-        opIzq = cuad[1]
-        opDer = cuad[2]
-        resultado = cuad[3]
+        print("Estoy en '='")
+        opIzq = cuad[1] # Direccion de memoria
+        opDer = cuad[2] # Siempre es None por el igual
+
+        # Revisar que exista en tabla de constantes, locales, globales
+        # Asignar valor encontrado o direccion encontrada en valorIzq o valorDer
+
+
+
         ##resultado a memoria
         valorIzq = 0
         valorDer = 0
 
+        # Solo sacamos tipo1 porque el otro siempre es None
+        type1=memoriaGlobal.getType(opIzq)
 
-        cuadruplo.pop(0)
-        ##contador
-        ##return contador        
-        print(cuadruplo[0])
-        '''
+        # Traer datos de la direccion
+        # Buscamos si no es global
+        if(type1 == 'int') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'float') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'char') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+        elif(type1 == 'bool') :
+            valorIzq = memoriaConstante.getValue(opIzq)
+            
+        resultado = valorIzq
+
+
+        #valorIzq = opIzq # Traer datos de la direccion
+        #valorDer = opDer      
+        return pos +1
+        
 
 
 
@@ -157,5 +235,11 @@ def ejecuta():
 
 
 
-if __name__ == "__main__":
-    ejecuta()        
+cont = 0
+while quadruples[cont][0] != 'END':
+    print(quadruples[cont])
+    
+    cont = ejecuta(quadruples[cont],cont)
+    print(cont)
+    #i = switch(quad[i], i)
+     
